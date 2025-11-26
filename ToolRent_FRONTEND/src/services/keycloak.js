40483588eb46a28c5dc55e4c8b514abc6b7cf00f
cristopher7 <cristopher.torres@usach.cl>
2025-11-25 23:56:54 -0300

    cambios en el docker compose

--- ToolRent_FRONTEND/src/services/keycloak.js
+++ ToolRent_FRONTEND/src/services/keycloak.js
@@ -1,9 +1,12 @@
 import Keycloak from "keycloak-js";
 
+const isDocker = window.location.hostname !== "localhost";
+
 const keycloak = new Keycloak({
-  url: "http://localhost:9090",
+  url: isDocker ? "http://keycloak:8080" : "http://localhost:9090",
   realm: "tingeso-realm",
-  clientId: "toolrent-frontend",
+  clientId: "toolrent-frontend"
 });
 
 export default keycloak;
+
